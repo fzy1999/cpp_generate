0424fd9f6c47370890ec079b65388e00d3539a48
fengzhaoyu <921797341@qq.com>
2023-05-12 11:42:42 +0000

    complicate example

--- data/exampleClass.h
+++ data/exampleClass.h
@@ -7,29 +7,37 @@ using std::string;
 class SecondClass;
 
 RTTR_REFLECT(WithNonPublic)
-class BottomClass {
-public:
+class BottomClass
+{
+ public:
   string name = "Buttom";
   // SecondClass* second;
 };
 
 RTTR_REFLECT(WithNonPublic)
-class SecondClass {
-public:
-  BottomClass *bottom;
+class SecondClass
+{
+ public:
+  BottomClass* bottom;
   string name = "Second";
   int32_t y = 88;
   // RTTR_ENABLE()
 };
 
 RTTR_REFLECT(WithNonPublic)
-class TopClass {
-public:
-  void set_second(SecondClass *_second) { second = _second; }
-  std::vector<SecondClass *> secplist;
-  SecondClass *second;
+class TopClass
+{
+ public:
+  class Top
+  {
+    int _x = 123;
+  };
+  void set_second(SecondClass* _second) { second = _second; }
+  std::vector<SecondClass*> secplist;
+  SecondClass* second;
   RTTR_REGISTRATION_FRIEND
-private:
+ private:
   string name = "top";
   int32_t x = 99;
+  Top top;
 };
