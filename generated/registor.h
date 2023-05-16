20b32333602c4233eb476a6526dfd539e3698f91
fengzhaoyu <921797341@qq.com>
2023-05-16 11:36:43 +0000

    add ast method analysis

--- generated/registor.h
+++ generated/registor.h
@@ -1,22 +1,44 @@
 #pragma once
-#include <rttr/registration>
-#include <rttr/type>
+#include "myrttr/type"
+#include "myrttr/registration"
+#include "../common.h"
 
 #include "/home/sxx/workSpace/cpp_generator/output/../data/exampleClass.h"
 
-RTTR_REGISTRATION {
-  struct TopClass {
-    std::vector<SecondClass *> secplist;
-    SecondClass *second;
-    string name;
-    int32_t x;
-  };
-  struct SecondClass {
-    BottomClass *bottom;
-    string name;
-    int32_t y;
-  };
-  struct BottomClass {
-    string name;
-  }
+
+RTTR_REGISTRATION{
+    rttr::registration::class_<io::IdHolder>("IdHolder")
+      .property("id", &io::IdHolder::id)
+      .property("derive_type", &io::IdHolder::derive_type);
+  rttr::registration::class_<TopClass>("TopClass")
+      .constructor<>()
+      
+      .property("secplist", &TopClass::secplist)
+      .property("second", &TopClass::second)
+      .property("name", &TopClass::name)
+      .property("x", &TopClass::x)
+      .property("top", &TopClass::top)
+      
+      ;
+        rttr::registration::class_<SecondClass>("SecondClass")
+      .constructor<>()
+      
+      .property("bottom", &SecondClass::bottom)
+      .property("name", &SecondClass::name)
+      .property("y", &SecondClass::y)
+      
+      ;
+        rttr::registration::class_<TopClass::Top>("TopClass::Top")
+      .constructor<>()
+      
+      .property("_x", &TopClass::Top::_x)
+      
+      ;
+        rttr::registration::class_<BottomClass>("BottomClass")
+      .constructor<>()
+      
+      .property("name", &BottomClass::name)
+      
+      ;
+      
 }
--- parser/action.h
+++ parser/action.h
@@ -8,39 +8,47 @@
 #include "clang/Frontend/FrontendActions.h"
 
 class Action : public ASTFrontendAction {
- public:
-  explicit Action(Context* ctx) : _ast_callback(ctx), _ctx(ctx) {
-    static const auto class_matcher = cxxRecordDecl(isDefinition(), unless(isExpansionInSystemHeader())).bind("c");
+public:
+  explicit Action(Context *ctx) : _ast_callback(ctx), _ctx(ctx) {
+    static const auto class_matcher =
+        cxxRecordDecl(isDefinition(), unless(isExpansionInSystemHeader()))
+            .bind("c");
     _finder.addMatcher(class_matcher, &_ast_callback);
-    static const auto enum_matcher = enumDecl(unless(isExpansionInSystemHeader())).bind("e");
+    static const auto enum_matcher =
+        enumDecl(unless(isExpansionInSystemHeader())).bind("e");
     _finder.addMatcher(enum_matcher, &_ast_callback);
+    static const auto method_matcher =
+        cxxMethodDecl(isDefinition(), unless(isExpansionInSystemHeader()))
+            .bind("m");
+    _finder.addMatcher(method_matcher, &_ast_callback);
   }
 
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance& compiler, StringRef /*in_file*/)
-  override {
+  std::unique_ptr<ASTConsumer>
+  CreateASTConsumer(CompilerInstance &compiler,
+                    StringRef /*in_file*/) override {
     // register macro handler
-    compiler.getPreprocessor().addPPCallbacks(std::make_unique<MacroCallback>(compiler.getSourceManager(),  //
-                                                                              compiler.getLangOpts(),       //
-                                                                              _ctx));
+    compiler.getPreprocessor().addPPCallbacks(
+        std::make_unique<MacroCallback>(compiler.getSourceManager(), //
+                                        compiler.getLangOpts(),      //
+                                        _ctx));
     // forward work to match finder
     return _finder.newASTConsumer();
   }
 
- private:
+private:
   MatchFinder _finder;
   AstCallback _ast_callback;
-  Context* _ctx;
+  Context *_ctx;
 };
 
 class ActionFactory : public tooling::FrontendActionFactory {
- public:
-  explicit ActionFactory(Context* ctx) : _ctx(ctx) {
-  }
+public:
+  explicit ActionFactory(Context *ctx) : _ctx(ctx) {}
 
   std::unique_ptr<FrontendAction> create() override {
     return std::make_unique<Action>(_ctx);
   }
 
- private:
-  Context* _ctx;
+private:
+  Context *_ctx;
 };
